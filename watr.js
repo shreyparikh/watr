801c39eab50f064c467bd8a6294230fec989589d
Andrew Frank <andrewfrank12@sbcglobal.net>
2021-01-23 23:41:03 -0600

    Update watr.js

--- watr.js
+++ watr.js
@@ -1,3 +1,19 @@
-function test(){
-	document.getElementById("button").innerHTML = "congrats";
+// break line
+var br = document.createElement("br");
+
+function create_form() {
+	// create form and set attributes
+	var form = document.createElement("form");
+	form.setAttribute("method", "post");
+	form.setAttribute("action", "submit.php");
+
+	// Create input elements
+	var meat = document.createElement("input");
+	var dairy = document.createElement("input");
+	meat.setAttribute("type", "radio");
+	dairy.setAttribute("type", "radio");
+
+	//var diet = document.createElement("label");
 }
+
+create_form();
